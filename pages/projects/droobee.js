de9b97dfb76a79a07499dcb91f8c24c2cd50c326
hugorithm <hugofsilv@gmail.com>
2022-08-01 16:34:06 +0100

    remove favicon

--- pages/projects/droobee.js
+++ pages/projects/droobee.js
@@ -3,8 +3,7 @@ import {
     Badge,
     Link,
     List,
-    ListItem,
-    AspectRatio
+    ListItem
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Title, ProjectImage, Meta } from '../../components/project';
