dfc0b520ad1027752a28dc49c92d25d8c6bf5a8a
hugorithm <hugofsilv@gmail.com>
2022-08-02 16:00:18 +0100

    fix typo

--- pages/projects/blockchain.js
+++ pages/projects/blockchain.js
@@ -18,7 +18,7 @@ const Project = () => (
             </Title>
             <P>
             This is the project that I chose for my Master's Dissertation.
-            It is a Blockchain Network based on Hyperledger Fabric that is used to track Healthcare patient data trought various beacons.
+            It is a Blockchain Network based on Hyperledger Fabric that is used to track Healthcare patient data through various beacons.
             It integrates a Web API in Node.js for asset creation, modification and has support for asset querying, as well as,
             various Blockchain monitoring software: Blockchain Explorer, Hyperledger Caliper, Prometheus and Grafana.
             It started at the Hospital of S. João in Oporto, where there were clear issues regarding data immutability and veracity.
