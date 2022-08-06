6c95b842b37cf8b0c52308efcf5ba70b6f86738a
hugorithm <hugofsilv@gmail.com>
2022-08-06 21:53:37 +0100

    refactor

--- pages/projects/blockchain.js
+++ pages/projects/blockchain.js
@@ -17,20 +17,20 @@ const Project = () => (
                 Blockchain Solution for Healthcare Data Tracking <Badge>2020</Badge>
             </Title>
             <P>
-            This is the project that I chose for my Master's Dissertation.
-            It is a Blockchain Network based on Hyperledger Fabric that is used to track Healthcare patient data through various beacons.
-            It integrates a Web API in Node.js for asset creation, modification and has support for asset querying, as well as,
-            various Blockchain monitoring software: Blockchain Explorer, Hyperledger Caliper, Prometheus and Grafana.
-            It started at the Hospital of S. João in Oporto, where there were clear issues regarding data immutability and veracity.
-            This project uses Blockchain Technology to ensure a tamper-proof, immutable, controlled, and secured way of storing 
-            data in order to achieve healthcare data veracity, privacy, and reliability.
-            You can check how I built this <Link href="/documents/Master_Thesis.pdf">here!</Link>
+                This is the project that I chose for my Master's Dissertation.
+                It is a Blockchain Network based on Hyperledger Fabric that is used to track Healthcare patient data through various beacons.
+                It integrates a Web API in Node.js for asset creation, modification and has support for asset querying, as well as,
+                various Blockchain monitoring software: Blockchain Explorer, Hyperledger Caliper, Prometheus and Grafana.
+                It started at the Hospital of S. João in Oporto, where there were clear issues regarding data immutability and veracity.
+                This project uses Blockchain Technology to ensure a tamper-proof, immutable, controlled, and secured way of storing
+                data in order to achieve healthcare data veracity, privacy, and reliability.
+                You can check how I built this <Link href="/documents/Master_Thesis.pdf">here!</Link>
 
             </P>
             <List ml={4} my={4}>
                 <ListItem>
                     <Meta>Website</Meta>
-                    <Link href="https://github.com/hugorithm/healthcare-tracking-blockchain">
+                    <Link href="https://github.com/hugorithm/Healthcare-tracking-blockchain">
                         https://github.com/hugorithm/Healthcare-tracking-blockchain <ExternalLinkIcon mx="2px" />
                     </Link>
                 </ListItem>
