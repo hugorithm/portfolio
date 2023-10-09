68e869a252da3a852ef3c1b215524cbafca35186
hugorithm <hugofsilv@gmail.com>
2023-10-09 15:48:24 +0100

    fix typo in quote

--- pages/projects/hopfencraft.js
+++ pages/projects/hopfencraft.js
@@ -20,7 +20,7 @@ const Project = () => (
             This project was so much fun to make 😁!
             I started this project to both learn and showcase my skills in the Java stack, as well as, React.
             I was curious about the Spring Framework and decided to give it a go. As it turns out I really liked it.
-            As for the frontend, I wanted to relearn React as it is a very <em>"javascripty"</em> functional oriented framework. 
+            As for the frontend, I wanted to relearn React as it is a very <em>&quot;javascripty&quot;</em> functional oriented framework. 
             I&apos;m really happy with how it turned out, and as a result, this is a project which I&apos;m really proud of!
             Please feel free to fork the project and contribute!
             </P>
