aa20434da4402fb986ce6026b58916c1b47cbd3a
hugorithm <hugofsilv@gmail.com>
2023-10-12 10:31:46 +0100

    move css to theme.js

--- libs/theme.js
+++ libs/theme.js
@@ -5,7 +5,66 @@ const styles = {
   global: props => ({
     body: {
       bg: mode('#c9deff', '#1a202c')(props)
-    }
+    },
+    "body.chakra-ui-dark p.typewriter": {
+      fontFamily: "Fira Code, monospace",
+      fontSize: "13px",
+      overflow: "hidden",
+      borderRight: "8px solid rgb(255, 255, 255)",
+      whiteSpace: "nowrap",
+      letterSpacing: ".15em",
+      maxWidth: "min-content",
+      overflow: "hidden",
+      animation: "typing 4.5s steps(60, end), blink-caret 1s step-end infinite",
+    },
+    "body.chakra-ui-light p.typewriter": {
+      fontFamily: "Fira Code, monospace",
+      fontSize: "13px",
+      overflow: "hidden",
+      borderRight: "8px solid rgb(53, 53, 53)",
+      whiteSpace: "nowrap",
+      letterSpacing: ".15em",
+      maxWidth: "min-content",
+      overflow: "hidden",
+      animation: "typing 4.5s steps(60, end), blink-caret-dark 1s step-end infinite",
+    },
+    "body.chakra-ui-dark::-webkit-scrollbar-thumb, -moz-scrollbar-thumb": {
+      backgroundColor: "#364158",
+      borderRadius: "8px",
+      cursor: "pointer",
+    },
+    "body.chakra-ui-light::-webkit-scrollbar-thumb, -moz-scrollbar-thumb": {
+      backgroundColor: "#3182ce",
+      borderRadius: "8px",
+      cursor: "pointer",
+    },
+    "::-webkit-scrollbar, -moz-scrollbar": {
+      width: "8px",
+      height: "10px",
+      cursor: "pointer",
+    },
+    "body.chakra-ui-dark::-webkit-scrollbar-track, -moz-scrollbar-track": {
+      boxShadow: "inset 2 2 6px rgba(0,0,0,.3)",
+      WebkitBoxShadow: "inset 2 2 6px rgba(0,0,0,.3)",
+      borderRadius: "1px",
+      backgroundColor: "#1a202c",
+      cursor: "pointer",
+    },
+    "body.chakra-ui-light::-webkit-scrollbar-track, -moz-scrollbar-track": {
+      boxShadow: "inset 2 2 6px rgba(0,0,0,.3)",
+      WebkitBoxShadow: "inset 2 2 6px rgba(0,0,0,.3)",
+      borderRadius: "1px",
+      backgroundColor: "#dee4e7",
+      cursor: "pointer",
+    },
+    ".profile-pic:hover": {
+      transform: "translate(5px, 5px)",
+      filter: "none",
+      mixBlendMode: "unset",
+    },
+    ".profile-pic": {
+      transition: "all 0.3s ease-in-out",
+    },
   })
 }
 
