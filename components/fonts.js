b5c6fd075b6e7d4bce143525b06e1394184d0a33
hugorithm <hugofsilv@gmail.com>
2023-10-16 00:50:41 +0100

    import JetBrains Mono font

--- components/fonts.js
+++ components/fonts.js
@@ -5,6 +5,7 @@ const Fonts = () => {
     <Global
       styles={`
                 @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
             `}
     ></Global>
   );
